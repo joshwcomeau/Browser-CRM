983ffa446f1f5680403ce9d7d187442817685593
Josh Comeau <joshwcomeau@gmail.com>
2014-04-03 16:03:21 -0400

    Changed stuff.

--- server.rb
+++ server.rb
@@ -11,50 +11,77 @@ end
 @@rolodex.new_contact("Joshua Comeau", "joshwcomeau@gmail.com", ["It's me! I created this neat little thing right?", "note 2"])
 @@rolodex.new_contact("James Dean", "singer@gmail.com", ["One of those celebrity people from before I was born."])
 @@rolodex.new_contact("Barack Obama", "president@usa.gov", ["43rd President of the United States. Should have hired me to make the ObamaCare website (I couldn't have done worse)."])
+@@app_name = app_name
 
+#### FUNCTIONS ####
+def gather_notes(data)
+  notes_fields = [:notes1, :notes2, :notes3, :notes4, :notes5]
+  notes = []
+  notes_fields.each do |field|
+    notes << params[field] unless params[field].empty?
+  end
+
+  notes
+end
+
+# redirect to('/contacts')
 
 #### ROUTES ####
 get "/" do  
-  @app_name = app_name
   erb :index 
 
 end
 
 get "/add" do    # View the 'add contact' page
-  @app_name = app_name
   erb :new
 end
 
 get "/contacts" do        # View all contacts
-  @app_name = app_name
+  if params.empty?
+    @results = @@rolodex.contacts
+    @header_msg = "Viewing All Contacts"
+  else
+    @results = @@rolodex.contacts.select do |contact|
+      string = contact.name + contact.email
+      contact.notes.each do |note|
+        string += note
+      end
+      string.downcase.include?(params[:search].downcase)
+    end
+    @header_msg = "Search Results:"
+    @results_num = @results.length
+  end
+
   erb :contacts
 end
 
-# get "/contacts/:id" do        # View a contact
-#   @contact = @@rolodex.grab_contact(:id.to_s)
-#   erb :view
-# end
+get "/contacts/:id" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
 
-get "/contacts/:id/edit" do   # Edit a contact
+get "/contacts/:id/edit" do   # View or Edit a contact
   id = params[:id].to_i
-  @app_name = app_name
   @contact = @@rolodex.grab_contact(id)
+
 
 
   erb :view
 end
 
 post "/contacts/confirm" do       # Post a new contact
-  @app_name = app_name
+  puts params
   @name = params[:first_name]
   @email = params[:email]
-  notes_fields = [:notes1, :notes2, :notes3, :notes4]
-  @notes = []
+  
+  @notes = gather_notes(params)
 
 
-  notes_fields.each do |field|
-    @notes << params[field] unless params[field].empty?
-  end
+  
 
   @@rolodex.new_contact(@name, @email, @notes)
   @id = @@rolodex.get_id_by_name(@name)
@@ -66,24 +93,16 @@ post "/contacts/savechanges" do
   puts params
   puts "/n/n/n"
   @contact = @@rolodex.grab_contact(params["id"].to_i)
-  @note_fields = ["note_1", "note_2", "note_3", "note_4", "note_5"]
-  @new_notes = []
 
-  @note_fields.each do |field|
-    if params[field] != '' || params[field] != nil
-      @new_notes << params[field]
-    end
-  end
-
+  @notes = gather_notes(params)
   @name = params["first_name"]
   @email = params["email"]
   @id = params["id"].to_i
 
-  # Validate Email
 
   @contact.name = @name
   @contact.email = @email
-  @contact.notes = @new_notes
+  @contact.notes = @notes
   
 
   erb :confirm
